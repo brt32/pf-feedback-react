b08b0681269fb8d77346ff8a37ed4e2cf846c6bc
Bartek <bartlomiej.proczkowski@gmail.com>
2022-01-09 00:17:49 +0100

    State before adding dummy backend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,20 @@
 import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import Spinner from "./shared/Spinner";
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>;
   }
 
   // with animation
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
